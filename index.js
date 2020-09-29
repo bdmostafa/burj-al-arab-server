f3cabc268d3382870612c5df21bad462d7b21ca7
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-29 20:07:32 +0600

    dotenv

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const admin = require('firebase-admin');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efifc.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 const port = 5000
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const serviceAccount = require("./burj-al-arab2020-firebase-adminsdk-kfiqa-7ff9c1bce9.json");
+const serviceAccount = require("./configs/burj-al-arab2020-firebase-adminsdk-kfiqa-7ff9c1bce9.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://burj-al-arab2020.firebaseio.com"
+    databaseURL: process.env.FIRE_DB
 });
 
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://arabianUser:arabianUser2020@cluster0.efifc.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(
     uri,
     { useUnifiedTopology: true },
@@ -52,21 +53,19 @@ client.connect(err => {
                     if (tokenEmail == req.query.email) {
                         bookingsCollection.find({ email: queryEmail })
                             .toArray((err, documents) => {
-                                res.statusCode(200).send(documents);
+                                res.status(200).send(documents);
                             })
                     }
                     else {
-                        res.statusCode(401).send('Unauthorized access');
+                        res.status(401).send('Unauthorized access');
                     }
                 }).catch((error) => {
-                    res.statusCode(401).send('Unauthorized access')
+                    res.status(401).send('Unauthorized access')
                 });
         } else {
-            res.statusCode(401).send('Unauthorized access');
+            res.status(401).send('Unauthorized access');
         }
     })
-
-
     // client.close();
 });
 
